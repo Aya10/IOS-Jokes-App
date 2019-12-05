4207bf14b47dfb9a2348e1bb3afe574cdfe05ded
Aya10 <47397624+Aya10@users.noreply.github.com>
2019-12-05 19:12:07 +0000

    Initial Commit

---
+++ JokesApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JokesApp
+//
+//  Created by Aya Aboutwerat on 05/12/2019.
+//  Copyright © 2019 Aya Aboutwerat. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
